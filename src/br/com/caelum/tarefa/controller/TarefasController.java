fbda1f47571eaffe0323adc8d4196a1adeaab374
bruno <bruno@bruno-H87-HD3>
2016-12-01 17:00:25 -0300

    Atualização Projeto

--- src/br/com/caelum/tarefa/controller/TarefasController.java
+++ src/br/com/caelum/tarefa/controller/TarefasController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.tarefa.dao.TarefaDAO;
 import br.com.caelum.tarefa.modelo.Tarefa;
@@ -17,61 +16,58 @@ import br.com.caelum.tarefa.modelo.Tarefa;
 @Controller
 public class TarefasController {
 
+	private final TarefaDAO dao;
+
+	public TarefasController(TarefaDAO dao) {
+		this.dao = dao;
+	}
+
 	@RequestMapping("novaTarefa")
 	public String form() {
-
 		return "tarefa/formulario";
 	}
 
 	@RequestMapping("adicionaTarefa")
 	public String adiciona(@Valid Tarefa tarefa, BindingResult result) {
-		
-		if(result.hasFieldErrors("descricao"))
+
+		if (result.hasFieldErrors("descricao"))
 			return "tarefa/formulario";
-		
-		TarefaDAO dao = new TarefaDAO();
+
 		dao.adiciona(tarefa);
-		
+
 		return "tarefa/adicionada";
 	}
-	
+
 	@RequestMapping("listaTarefas")
-	public String lista(Model model){
-		TarefaDAO dao = new TarefaDAO();
+	public String lista(Model model) {
 		List<Tarefa> tarefas = dao.lista();
 		model.addAttribute("tarefas", tarefas);
 		return "tarefa/lista";
 	}
-	
+
 	@RequestMapping("removeTarefa")
-	public String remove(Tarefa tarefa){
-		TarefaDAO dao = new TarefaDAO();
+	public String remove(Tarefa tarefa) {
 		dao.remove(tarefa);
 		return "forward:listaTarefas";
 	}
-	
-	
+
 	@RequestMapping("mostraTarefa")
-	public String mostra(Long id, Model model){
-		TarefaDAO dao = new TarefaDAO();
+	public String mostra(Long id, Model model) {
 		model.addAttribute("tarefa", dao.buscaPorId(id));
 		return "tarefa/mostra";
 	}
-	
+
 	@RequestMapping("alteraTarefa")
-	public String altera(Tarefa tarefa){
-		TarefaDAO dao = new TarefaDAO();
+	public String altera(Tarefa tarefa) {
 		dao.altera(tarefa);
 		return "forward:listaTarefas";
 	}
-	
+
 	@RequestMapping("finalizaTarefa")
-	public void finaliza(Long id, HttpServletResponse response){
-		TarefaDAO dao = new TarefaDAO();
+	public void finaliza(Long id, HttpServletResponse response) {
 		dao.finaliza(id);
 		response.setStatus(200);
-		
+
 	}
-	
 
 }
